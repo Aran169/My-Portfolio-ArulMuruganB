8c600b9caec674aa6c5a703876c9e6704a6d48b2
Aran169 <muruganarul1693@gmail.com>
2024-11-20 13:31:54 +0530

    updates

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,11 +5,18 @@ import BT from "./../../assets/BudgetTrackerScreenshot.png";
 import ProjectCard from "../../common/ProjectCard";
 import LM from "./../../assets/bmsphoto.png";
 import MP from "./../../assets/MP.png";
+import smart from './../../assets/smart.png';
 function Projects() {
   return (
     <section id='project' className={styles.container}>
       <h1 className='sectionTitle'>Projects</h1>
       <div className={styles.projectsContainer}>
+      <ProjectCard
+          src={smart}
+          link='https://github.com/Aran169/City-Monitor'
+          h3='Smart City Monitor'
+          P='ReactJS | NodeJS | MongoDB | Tableau'
+        />
         <ProjectCard
           src={XO}
           link='https://github.com/Aran169/X-O-game-using-Mini-Max-algorithm-Java'
